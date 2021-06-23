1c3fc2c878d6a4ca067cfa8903e1577b9c4fb4e2
AmmarDagha <ammardagha@gmail.com>
2021-06-23 15:50:25 +0500

    adding exercises file

---
+++ Exercises.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+product = num1*num2
+print ("The product of the two numbers is: ",product)
+if product>1000:
+    sum = num1+num2
+    print ("The sum of the two numbers is: ", sum)
